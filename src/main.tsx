195fc31f38ad1441ab93e4c10f85be3561284fba
Matheus Pedroni <matheusp@mo.co.uk>
2025-09-15 13:44:15 +0100

    feat: components and dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+/** biome-ignore-all lint/style/noNonNullAssertion: react root */
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);

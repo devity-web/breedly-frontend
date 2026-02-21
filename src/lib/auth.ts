de0744fd6d6f674c2931da20aefcf731396bbffe
Matheus Pedroni <pedroni.dev@gmail.com>
2026-02-21 20:39:22 +0000

    feat: fix user avatar

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
 import {createAuthClient} from 'better-auth/react';
+import {getBaseUrl} from '@/utils/get-base-url';
 
 export const authClient = createAuthClient({
-  baseURL: 'http://localhost:3000',
+  baseURL: `${getBaseUrl()}/auth`,
 });
